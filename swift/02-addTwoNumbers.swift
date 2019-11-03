7c9781299455f0b623e3cf32fe5e121f19e2be1b
Fernando Florez <arrobaunferna@gmail.com>
2019-11-02 21:54:49 -0500

    [Swift] addTwoNumbers problem solved

--- swift/02-addTwoNumbers.swift
+++ swift/02-addTwoNumbers.swift
@@ -1,70 +1,113 @@
-import Foundation
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public var val: Int
- *     public var next: ListNode?
- *     public init(_ val: Int) {
- *         self.val = val
- *         self.next = nil
- *     }
- * }
- */
+ * You are given two non-empty linked lists representing two non-negative integers. 
+ * The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ * Source: https://leetcode.com/problems/add-two-numbers/
+*/
 
-public class ListNode {
-    public var val: Int
-    public var next: ListNode?
-    public init(_ val: Int) {
-        self.val = val
+import Foundation
+
+class Node {
+    var value: Int!
+    var next: Node?
+
+    init(_ value: Int) {
+        self.value = value
         self.next = nil
     }
-}
 
-extension ListNode {
-    func insertAtStart(_ listNode: ListNode?) {
+    func insertAt(start value: Int) {
+        var newValue = Node(value)
+        let aux = Node(self.value)
+        aux.next = self.next
+
+        newValue.next = aux
+        self.value = newValue.value
+        self.next = newValue.next
+    }
+    
+    func insertAt(end value: Int) {
+        let newValue = Node(value)
+        var lastItem: Node? = self.next
+
+        while lastItem?.next != nil {
+            lastItem = lastItem?.next
+        }
+
+        if lastItem == nil {
+            self.next = newValue
         
+        } else {
+            lastItem?.next = newValue
+        }
     }
 
-    func printList() {
-        var iter: ListNode? = self
+    func printMe() {
+        var iter: Node? = self
 
         while iter != nil {
-            if let value = iter?.val {
-                print(value)
-            }
-
+            print(iter?.value)
             iter = iter?.next
         }
     }
 }
 
 class Solution {
-    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        var l1Handler: ListNode? = l1
-        var l2Handler: ListNode? = l2
+    func addTwoNumbers(_ l1: Node?, _ l2: Node?) -> Node? {
+        var l1Handler: Node? = l1
+        var l2Handler: Node? = l2
+        var result: Node? = nil
+        var surplus = 0
         
         while l1Handler != nil && l2Handler != nil {
+            var number1: Int! = l1Handler!.value
+            var number2: Int! = l2Handler!.value
             
+            let sum = number1 + number2 + surplus
+            var numToAdd = sum
+
+            if sum > 9 {
+                numToAdd = numToAdd % 10
+                surplus = Int(sum / 10)
+            
+            } else {
+                surplus = 0
+            }
+
+            if result == nil {
+                result = Node(numToAdd)
+            
+            } else {
+                result?.insertAt(start: numToAdd)
+            }
+
             l1Handler = l1Handler?.next
             l2Handler = l2Handler?.next
+        }  
+
+        while surplus > 0 {
+            var numToAdd = surplus % 10
+            surplus = Int(surplus / 10)
+
+            surplus -= numToAdd
+
+            result?.insertAt(start: numToAdd)
         }
 
-        return nil
+        return result
     }
 }
 
-var list1 = ListNode(2)
-var list1A = ListNode(4)
-var list1B = ListNode(3)
+let list = Node(2)
+list.insertAt(end: 4)
+list.insertAt(end: 3)
 
-list1.next = list1A
-list1A.next = list1B
+let list2 = Node(5)
+list2.insertAt(end: 6)
+list2.insertAt(end: 4)
 
-list1.printList()
+let solution = Solution()
+let result = solution.addTwoNumbers(list, list2)
 
-var list2 = ListNode(5)
-var list2A = ListNode(6)
-var list2B = ListNode(4)
-
-list2.next = list2A
-list2A.next = list2B
+result?.printMe()
